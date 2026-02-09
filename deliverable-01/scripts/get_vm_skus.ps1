2711d38f1c6c2c613b47b065182e5acc5f67be36
Evan Brooks <70245295+eb613819@users.noreply.github.com>
2026-02-09 06:35:40 -0500

    Filter SKUs to include only Standard_B types
    Added filtering for Standard_B SKUs in get_vm_skus.ps1.

--- deliverable-01/scripts/get_vm_skus.ps1
+++ deliverable-01/scripts/get_vm_skus.ps1
@@ -19,6 +19,8 @@ foreach ($region in $allowedRegions) {
     # Get all Standard_B[12]* SKUs for this region
     $skus = Get-AzComputeResourceSku -Location $region |
         Where-Object { $_.ResourceType -like 'virtualMachines'}
+        # For just Standard_B
+        # Where-Object {$_.Name -like 'Standard_B[12][a-z]*'}
         
     if ($DebugMode) { Write-Host "Found SKUs:" ($skus.Name -join ", ") }
 
